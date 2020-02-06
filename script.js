0942dfa70e507b08a2884fd0f819ce878c661a31
courtlandmessam <messamcourtland@gmail.com>
2020-02-06 10:38:28 -0500

    testing post using fetch api

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hey Im working");
+
+fetch(`https://rel.ink/api/links/`, {
+    method: 'post',
+    headers: {
+     "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+   },
+    body: {"url": "https://developers.google.com/web/updates/2015/03/introduction-to-fetch"}
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+    console.log('Request failed', error);
+  });
