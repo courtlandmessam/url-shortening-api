cc552f760358120e3b439aab762833a70d7ae34f
courtlandmessam <messamcourtland@gmail.com>
2020-02-06 11:19:28 -0500

    fixed post functionality and added linkLog array for storing instances of links

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 console.log("Hey Im working");
 
+let linkLog = [];
+
 fetch(`https://rel.ink/api/links/`, {
     method: 'post',
     headers: {
-     "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+     "Content-type": "application/json; charset=UTF-8"
    },
-    body: {"url": "https://developers.google.com/web/updates/2015/03/introduction-to-fetch"}
+    body: '{"url": "https://developers.google.com/web/updates/2015/03/introduction-to-fetch"}'
   })
   .then(function(response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    linkLog.push(data);
+    console.log(linkLog);
   })
   .catch(function (error) {
     console.log('Request failed', error);
